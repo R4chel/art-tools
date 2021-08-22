ece1245722f2139554875a68df5ecf171d5bd981
R4chel <rae84@cornell.edu>
2021-08-22 16:01:54 -0400

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{:?}", args); 
+    println!("HERE!"); 
 }
